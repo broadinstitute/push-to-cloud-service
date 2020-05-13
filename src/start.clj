52724b6b1132c2c980c811a0dc77b497738d3193
Rex Wang <rexwangcc@gmail.com>
2020-05-13 17:06:17 -0400

    Add a POC message loop example.

--- src/start.clj
+++ src/start.clj
@@ -1,19 +1,21 @@
 (ns start
   (:gen-class)
-  (:require [clj-time.core   :as t])
+  (:require [util])
   (:import  [org.apache.activemq ActiveMQSslConnectionFactory]
             [javax.jms TextMessage DeliveryMode Session]))
 
 (defn with-push-to-cloud-jms-connection
-  "CALL (use connection destination) for the push-to-cloud JMS queue with URL, QUEUE, USERNAME and PASSWORD."
-  [url queue username password call]
-  (let [factory (new ActiveMQSslConnectionFactory url)]
+  "CALL (use connection destination) for the push-to-cloud JMS queue with ENVIRONMENT."
+  [environment call]
+  (let [vault-path (format "secret/dsde/gotc/%s/activemq/logins/zamboni" environment)
+        {:keys [url username password queue]} (util/vault-secrets vault-path)
+        factory (new ActiveMQSslConnectionFactory url)]
     (with-open [connection (.createQueueConnection factory username password)]
       (call connection queue))))
 
 (defn send-text
-  "Enqueue the TEXT with PROPERTIES map to push-to-cloud JMS queue with URL, QUEUE, USERNAME and PASSWORD."
-  [url queue username password text properties]
+  "Enqueue the TEXT with PROPERTIES map to push-to-cloud JMS queue with ENVIRONMENT."
+  [environment text properties]
   (letfn [(add-property [^TextMessage message k v] (.setStringProperty message k v))
           (send [connection destination]
             (let [transacted? true]
@@ -27,11 +29,11 @@
                     (.start connection)
                     (.send producer message)
                     (.commit session))))))]
-    (with-push-to-cloud-jms-connection url queue username password send)))
+    (with-push-to-cloud-jms-connection environment send)))
 
 (defn receive-text
-  "Nil on timeout or the text from a message from the push-to-cloud JMS queue with URL, QUEUE, USERNAME and PASSWORD"
-  [url queue username password]
+  "Nil on timeout or the text from a message from the push-to-cloud JMS queue with ENVIRONMENT."
+  [environment]
   (letfn [(receive [connection destination]
             (let [transacted? false
                   timeout 10000]
@@ -40,21 +42,35 @@
                   (with-open [consumer (.createConsumer session queue)]
                     (.start connection)
                     (.receive consumer timeout))))))]
-    (with-push-to-cloud-jms-connection url queue username password receive)))
+    (with-push-to-cloud-jms-connection environment receive)))
 
 (defn plus
   "Return the sum of A and B. Just for testing."
   [^Integer a ^Integer b]
   (+ a b))
 
+(defn message-loop
+  "A blocking message loop that periodically do something."
+  [environment]
+  (loop []
+    (let [_ (send-text environment "hornet" {"hello" "world!"})]
+      (when-let [messageText (receive-text environment)]
+        (println (.getText messageText))
+        (doseq [[k v] (.getProperties messageText)]
+          (prn k v))
+        (recur)))))
+
 (comment
-  (let [queue     "wfl.broad.pushtocloud.enqueue.dev"
-        url       "get this from the WFL environments.clj!!"
-        vault     "secret/dsde/gotc/dev/activemq/logins/zamboni"
-        username  "get this from above vault!!"
-        password  "get this from above vault!!"]
-    (send-text url queue username password "hornet" {"hello" "world!"})
-    (.getText (receive-text url queue username password))))
+  (message-loop "dev")
+
+  (let [environment "dev"]
+    ;;(send-text environment "hornet" {"hello" "world!"})
+    (when-let [msg (receive-text environment)]
+      (.getText msg)))
+
+  (util/reach-out-and-touch-everyone {:hello "world!"} []))
 
 (defn -main []
-  (println "Hello world!"))
+  (println "Hello world!")
+  (let [environment (System/getenv "environment")]
+    (message-loop environment)))
