60ff9b18dc756fa19e535f50052c1fd12e59196b
Edmund Higham <edhigham@gmail.com>
2020-06-26 18:25:43 +0100

    never build with cpcache

--- ops/build.sh
+++ ops/build.sh
@@ -3,8 +3,4 @@
 # Build Uberjar with --main-class option. See for more
 # information, see https://github.com/tonsky/uberdeps
 
-if [ ${USE_CPCACHE-1} -eq 0 ]; then
-    CLJ_FLAGS=-Sforce
-fi
-
-clojure $CLJ_FLAGS -A:uberdeps --main-class ptc.start
+clojure -Sforce -A:uberdeps --main-class ptc.start
