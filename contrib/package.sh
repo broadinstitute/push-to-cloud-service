807b0c3bc7c84ba27c5cd9f2fb928993a74f00d3
Edmund Higham <ehigham@users.noreply.github.com>
2020-07-22 19:33:56 +0100

    Create deployable systemd unit (#14)
    * remove unused jenkins scripts
    
    * create systemd user unit
    
    * be nice to our MacOS users
    
    * include install script in release artifact
    
    * update service file after @tbl3rd's suggestion
    
    * error when VERSION is not defined

---
+++ contrib/package.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+set -e
+
+bold() { printf '\e[1;1m%-6s\e[m\n' "$*"; }
+red() { printf '\e[1;91m%-6s\e[m\n' "$*"; }
+error() {
+    >&2 echo -e $(bold $(basename ${0}):) $(red $(bold 'error:')) ${1}
+}
+
+validate() {
+    local var="$1"
+    if [ -z "${!var}" ]; then
+        error "${var} must be set!"
+        exit 1
+    fi
+}
+
+main() {
+    validate VERSION
+    local release="push-to-cloud-service-${VERSION}"
+    local out="derived/${release}"
+
+    mkdir -p "${out}"
+
+    echo "${VERSION}" > "${out}/version"
+
+    cp in/install.sh.in "${out}/install.sh"
+    chmod +x "${out}/install.sh"
+
+    cp LICENSE "${out}"
+
+    mkdir -p "${out}/in/unit"
+    cp in/unit/push-to-cloud.service.in "${out}/in/unit"
+
+    mkdir -p "${out}/lib"
+    cp target/push-to-cloud-service.jar "${out}/lib"
+
+    if [[ -n "${NOTES}" ]]; then
+        mkdir -p "${out}/docs"
+        cp "${NOTES}" "${out}/docs"
+    fi
+
+    pushd derived
+    tar -czvf "${release}".tar.gz "${release}"
+    popd
+}
+
+main "$@"
