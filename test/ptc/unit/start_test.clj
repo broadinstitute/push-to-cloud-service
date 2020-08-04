35913466dc920853d825e8b0fa071a17c5cb455a
Tom Lyons <tbl@broadinstitute.org>
2020-08-04 17:32:41 -0400

    10 tests, 51 assertions, 0 failures.

--- test/ptc/unit/start_test.clj
+++ test/ptc/unit/start_test.clj
@@ -2,7 +2,8 @@
   (:require [clojure.test :refer [deftest is testing]]
             [clojure.edn  :as edn]
             [clojure.walk :as walk]
-            [ptc.start    :as start])
+            [ptc.start    :as start]
+            [ptc.util.jms :as jms])
   (:import [org.apache.activemq ActiveMQSslConnectionFactory]))
 
 ;; Local testing for ActiveMQ
@@ -19,34 +20,28 @@
 
 (def message
   "Example JMS message for testing."
-  (edn/read-string (slurp "test/data/test_msg.edn")))
+  (edn/read-string (slurp "test/data/good-jms.edn")))
 
 (deftest produce-consume-message
-  (let [message-text  (prn-str             (:headers message))
-        message-props (walk/stringify-keys (:properties message))]
+  (let [text "consume"
+        properties (::jms/Properties (jms/encode message))]
     (letfn [(produce-consume [connection queue]
-              (start/produce connection queue message-text message-props)
+              (start/produce connection queue text properties)
               (start/consume connection queue))]
       (if-let [msg (with-test-jms-connection produce-consume)]
         (testing "Message is not nil and can be properly consumed"
-          (is (= (:headers message)
-                 (edn/read-string (.getText msg))))
-          (doseq [[k v] (.getProperties msg)]
-            (is (= (get (:properties message) (keyword k))
-                   v))))
+          (is (= text (.getText msg)))
+          (is (apply = (map ::jms/Properties [message (jms/ednify msg)]))))
         (is false)))))
 
 (deftest produce-peek-message
-  (let [message-text  (prn-str             (:headers message))
-        message-props (walk/stringify-keys (:properties message))]
+  (let [text "peek"
+        properties (::jms/Properties (jms/encode message))]
     (letfn [(produce-peek [connection queue]
-              (start/produce connection queue message-text message-props)
+              (start/produce connection queue text properties)
               (start/peek-message connection queue))]
       (if-let [msg (with-test-jms-connection produce-peek)]
         (testing "Message is not nil and can be properly consumed"
-          (is (= (:headers message)
-                 (edn/read-string (.getText msg))))
-          (doseq [[k v] (.getProperties msg)]
-            (is (= (get (:properties message) (keyword k))
-                   v))))
+          (is (= text (.getText msg)))
+          (is (apply = (map ::jms/Properties [message (jms/ednify msg)]))))
         (is false)))))
