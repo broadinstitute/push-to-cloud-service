6a2b4a104d03d1dc7893cdf9b23f4cb45a0a819e
Tom Lyons <tbl3rd@gmail.com>
2020-08-19 18:20:33 -0400

    Maybe simpler?

--- test/ptc/tools/wfl.clj
+++ test/ptc/tools/wfl.clj
@@ -5,44 +5,43 @@
             [ptc.util.misc     :as misc]))
 
 (defn get-aou-workloads
-  "Query WFL for AllOfUsArrays workloads"
+  "Return the AllOfUsArrays workloads from WFL at WFL-URL."
   [wfl-url]
-  (let [auth-header (misc/get-auth-header!)
-        response (client/get (str wfl-url "/api/v1/workload")
-                             {:headers auth-header})]
-    (letfn [(array-workload? [workload]
-              (= (:pipeline workload) "AllOfUsArrays"))]
-      (->> (:body response)
-           (misc/parse-json-string)
-           (filter array-workload?)))))
+  (letfn [(aou? [workload] (= (:pipeline workload) "AllOfUsArrays"))]
+    (-> (str wfl-url "/api/v1/workload")
+      (client/get {:headers (misc/get-auth-header!)})
+      :body misc/parse-json-string
+      (->> (filter aou?)))))
 
-(defn is-aou-workflow?
-  "Check if a WORKFLOW has a specific CHIPWELL-BARCODE and ANALYSIS-VERSION."
-  [chipwell-barcode analysis-version workflow]
-  (and (= (:chip_well_barcode workflow) chipwell-barcode)
-       (= (:analysis_version_number workflow) analysis-version)))
+(defn aou-uuid
+  "Return a semipredicate that returns nil or the UUID of WORKFLOW if it
+  has CHIPWELL-BARCODE ANALYSIS-VERSION-NUMBER."
+  [chipwell-barcode analysis-version-number]
+  (let [match? (juxt :chip_well_barcode :analysis_version_number)]
+    (fn [workflow] (when (= [chipwell-barcode analysis-version-number]
+                           (match? workflow))
+                     (:uuid workflow)))))
 
-(defn get-workflow-id
-  "Get the AllOfUsArrays workflow started in WFL-URL by its CHIPWELL-BARCODE and ANALYSIS-VERSION."
-  [wfl-url chipwell-barcode analysis-version]
-  (remove nil? (for [workload (get-aou-workloads wfl-url)]
-                 (let [workflows (:workflows workload)]
-                   (if (seq workflows)
-                     (->> workflows
-                          (filter #(is-aou-workflow? chipwell-barcode analysis-version %))
-                          (first)
-                          (:uuid)))))))
+(defn get-aou-workflow-ids
+  "Return UUIDs of workflows at WFL-URL with CHIPWELL-BARCODE and
+  ANALYSIS-VERSION-NUMBER."
+  [wfl-url chipwell-barcode analysis-version-number]
+  (let [match? (aou-uuid chipwell-barcode analysis-version-number)]
+    (->> wfl-url
+      get-aou-workloads
+      (mapcat :workflows)
+      (keep match?))))
 
 (defn wait-for-workflow-creation
-  "Wait for a workflow with CHIPWELL-BARCODE and ANALYSIS-VERSION to appear in an AllOfUsArrays workload in WFL-URL."
-  [wfl-url chipwell-barcode analysis-version]
-  (loop [wfl-url wfl-url
-         chipwell-barcode chipwell-barcode
-         analysis-version analysis-version]
-    (let [seconds 15
-          workflow-id (get-workflow-id wfl-url chipwell-barcode analysis-version)]
-      (if (nil? (first workflow-id))
-        (do (log/infof "Sleeping %s seconds" seconds)
-            (misc/sleep-seconds seconds)
-            (recur wfl-url chipwell-barcode analysis-version))
-        (first (get-workflow-id wfl-url chipwell-barcode analysis-version))))))
+  "Wait for a workflow with CHIPWELL-BARCODE and ANALYSIS-VERSION-NUMBER
+  to appear in an AllOfUsArrays workload in WFL-URL."
+  [wfl-url chipwell-barcode analysis-version-number]
+  (let [seconds 15]
+    (loop []
+      (let [ids (get-aou-workflow-ids
+                  wfl-url chipwell-barcode analysis-version-number)]
+        (if (empty? ids)
+          (do (log/infof "Sleeping %s seconds" seconds)
+              (misc/sleep-seconds seconds)
+              (recur))
+          (first ids))))))
