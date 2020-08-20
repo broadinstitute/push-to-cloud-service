31b20594d5976770587502c8212f0897c233aefc
Saman Ehsan <sehsan@broadinstitute.org>
2020-08-20 09:40:13 -0400

    [GH-814] End-to-end test for AOU processing (#23)
    * WIP end-to-end test
    
    * Update end-to-end test
    
    - Add function to upload sample
      to the dev JMS queue (to be
      replaced by queue alias)
    - Verify files have been uploaded
    - Wait for workflow id in WFL
    - Wait for workflow success in
      Cromwell
    
    * Move system test location
    
    * Fix linting
    
    * Organize test code
    
    * Update test strings and assertions
    
    * Fix linting
    
    * Remove stray comment
    
    * Use arrays data for system test
    
    Also set some vars based on the
    environment
    
    * Make system test use PTC queue
    
    * Update test data
    
    * Fix prod wfl url
    
    * Wait for test to upload files
    
    * Remove old code
    
    * Randomize analysis-version
    
    Instead of the chipwell barcode,
    which is used by the arrays
    pipeline.
    
    * Reorganize gcs-related functions
    
    * Use analysis version from message
    
    Previously queue-messages was changing
    the analysis version number in the message
    but the end-to-end test needs to know this
    information to find the analysis workflow
    in WFL.
    
    * Fix checking for uploaded files
    
    * Capitalize env variables
    
    * Add error messages to test
    
    * Update test/ptc/tools/cromwell.clj
    
    Co-authored-by: Jack Warren <jack@jackwarren.info>
    
    * Rename alias e2e
    
    * Fix rebase
    
    * Fix test assertions
    
    * Maybe simpler?
    
    * Code clean-up
    
    * Lift get into fetch!
    
    * Fuss.
    
    * Remove redundant test assertions
    
    * Replace sleep-seconds fn
    
    * Read queue env from args
    
    * Rename for clarity
    
    * Fix linting errors
    
    * Fix integration test
    
    * Consolidate shared test code
    
    * Fix linting
    
    Co-authored-by: Jack Warren <jack@jackwarren.info>
    Co-authored-by: Tom Lyons <tbl3rd@gmail.com>

---
+++ test/ptc/tools/wfl.clj
@@ -0,0 +1,48 @@
+(ns ptc.tools.wfl
+  "Utility functions for WFL."
+  (:require [clj-http.client :as client]
+            [clojure.tools.logging :as log]
+            [ptc.util.misc     :as misc])
+  (:import [java.util.concurrent TimeUnit]))
+
+(defn get-aou-workloads
+  "Return the AllOfUsArrays workloads from WFL at WFL-URL."
+  [wfl-url]
+  (letfn [(aou? [workload] (= (:pipeline workload) "AllOfUsArrays"))]
+    (-> (str wfl-url "/api/v1/workload")
+        (client/get {:headers (misc/get-auth-header!)})
+        :body misc/parse-json-string
+        (->> (filter aou?)))))
+
+(defn aou-uuid
+  "Return a semipredicate that returns nil or the UUID of WORKFLOW when
+  it has CHIPWELL-BARCODE ANALYSIS-VERSION-NUMBER."
+  [chipwell-barcode analysis-version-number]
+  (let [match? (juxt :chip_well_barcode :analysis_version_number)]
+    (fn [workflow] (when (= [chipwell-barcode analysis-version-number]
+                            (match? workflow))
+                     (:uuid workflow)))))
+
+(defn get-aou-workflow-ids
+  "Return UUIDs of workflows at WFL-URL with CHIPWELL-BARCODE and
+  ANALYSIS-VERSION-NUMBER."
+  [wfl-url chipwell-barcode analysis-version-number]
+  (let [match? (aou-uuid chipwell-barcode analysis-version-number)]
+    (->> wfl-url
+         get-aou-workloads
+         (mapcat :workflows)
+         (keep match?))))
+
+(defn wait-for-workflow-creation
+  "Wait for a workflow with CHIPWELL-BARCODE and ANALYSIS-VERSION-NUMBER
+  to appear in an AllOfUsArrays workload in WFL-URL."
+  [wfl-url chipwell-barcode analysis-version-number]
+  (letfn [(fetch! [] (get-aou-workflow-ids
+                      wfl-url chipwell-barcode analysis-version-number))]
+    (let [seconds 15]
+      (loop [ids (fetch!)]
+        (if (empty? ids)
+          (do (log/infof "Sleeping %s seconds" seconds)
+              (.sleep TimeUnit/SECONDS seconds)
+              (recur (fetch!)))
+          (first ids))))))
