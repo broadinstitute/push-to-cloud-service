751b110ba828df8881929abf8ce1a4d4232922c9
Tom Lyons <tbl3rd@gmail.com>
2020-09-11 14:08:23 -0400

    Update JMS tests.

--- test/ptc/integration/jms_test.clj
+++ test/ptc/integration/jms_test.clj
@@ -39,32 +39,32 @@
         bad      (jms-tools/fix-paths "./test/data/bad-jms.edn")
         good     (jms-tools/fix-paths "./test/data/good-jms.edn")
         missing  (-> good (data/diff bad) first (get-in path) keys first
-                     (->> (str jms/missing-keys-message ".*"))
-                     re-pattern)
+                   (->> (str jms/missing-keys-message ".*"))
+                   re-pattern)
         workflow (get-in good path)]
     (with-temporary-gcs-folder folder
       (jms-tools/with-test-queue-connection
         (fn [connection queue]
           (testing "a BAD message"
             (start/produce connection queue
-                           "BAD" (::jms/Properties (jms/encode bad)))
+              "BAD" (::jms/Properties (jms/encode bad)))
             (let [msg (start/consume connection queue)]
               (is (thrown-with-msg? IllegalArgumentException missing
-                                    (jms/handle-message folder (jms/ednify msg))))
+                    (jms/handle-message folder (jms/ednify msg))))
               (is (empty? (->> folder
-                               gcs/parse-gs-url
-                               (apply gcs/list-objects))))))
+                            gcs/parse-gs-url
+                            (apply gcs/list-objects))))))
           (testing "a GOOD message"
             (start/produce connection queue
-                           "GOOD" (::jms/Properties (jms/encode good)))
+              "GOOD" (::jms/Properties (jms/encode good)))
             (let [msg    (start/consume connection queue)
                   [params ptc] (jms/handle-message folder (jms/ednify msg))
                   {:keys [notifications]} (gcs/gcs-edn ptc)
                   {:keys [::jms/chip ::jms/push]} jms/wfl-keys->jms-keys
-                  push   (-> (jms/handle-existing-cloud-paths jms/push-or-copy-keys push workflow)
-                             (->> (merge chip))
-                             keys
-                             (->> (apply juxt)))
+                  push   (-> workflow jms/wfl-keys->jms-keys-for ::jms/push
+                           (->> (merge chip))
+                           keys
+                           (->> (apply juxt)))
                   inputs (remove nil? (push (first notifications)))
                   pushed (into [params ptc] inputs)
                   gcs    (gcs/list-gcs-folder folder)]
