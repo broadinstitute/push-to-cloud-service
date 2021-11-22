04a54a042ea206e205152895c5a04d213b9b2a98
Tom Lyons <tbl3rd@gmail.com>
2021-11-22 14:17:27 -0500

    Remove the use-on-prem-idat-paths test.

--- test/ptc/unit/jms_test.clj
+++ test/ptc/unit/jms_test.clj
@@ -5,20 +5,10 @@
             [ptc.start :as start]
             [ptc.tools.jms :as jms-tools]
             [ptc.util.gcs :as gcs]
-            [ptc.util.jms :as jms]))
+            [ptc.util.jms :as jms]
+            [ptc.util.misc :as misc]))
 
 (def read-jms-message (comp edn/read-string slurp))
-
-(deftest use-on-prem-idat-paths
-  (testing "WFL uses the on-prem idat files if they do not exist in the cloud."
-    (let [jms (read-jms-message "./test/data/good-jms.edn")
-          workflow (get-in jms [::jms/Properties :payload :workflow])
-          {:keys [cloudGreenIdatPath cloudRedIdatPath]} workflow
-          push (::jms/push jms/wfl-keys->jms-keys)]
-      (is (not (gcs/gcs-object-exists? cloudGreenIdatPath)))
-      (is (not (gcs/gcs-object-exists? cloudRedIdatPath)))
-      (is (= (:green_idat_cloud_path push) :greenIDatPath))
-      (is (= (:red_idat_cloud_path   push) :redIDatPath)))))
 
 (deftest use-cloud-idat-paths
   (testing "WFL is able to find cloud idat paths if they exist."
