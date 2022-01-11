282da5fca14d2de8d62327da1c8d62a32353247a
Tom Lyons <tbl3rd@gmail.com>
2022-01-11 14:47:30 -0500

    Test GDA-8v1-0_D1 use.

--- test/ptc/unit/jms_test.clj
+++ test/ptc/unit/jms_test.clj
@@ -16,10 +16,17 @@
           prefix "gs://broad-gotc-dev-wfl-ptc-test-inputs"]
       (is (#'jms/find-input-or-throw prefix workflow :greenIDatPath)))))
 
+(deftest use-GDA-8v1-0_D1-chip
+  (testing "Can find cloud idats under an old chip."
+    (let [jms (read-jms-message "./test/data/GDA-8v1-0_D1-jms.edn")
+          workflow (get-in jms [::jms/Properties :payload :workflow])
+          prefix "gs://broad-gotc-dev-wfl-ptc-test-inputs"]
+      (is (#'jms/find-input-or-throw prefix workflow :greenIDatPath)))))
+
 (deftest test-message-id-equality
-  (let [msg       (edn/read-string (slurp "test/data/test_msg.edn"))
-        different (edn/read-string (slurp "test/data/test_msg_diff.edn"))
-        same      (edn/read-string (slurp "test/data/test_msg_same.edn"))]
+  (let [msg       (read-jms-message "./test/data/test_msg.edn")
+        different (read-jms-message "./test/data/test_msg_diff.edn")
+        same      (read-jms-message "./test/data/test_msg_same.edn")]
     (testing "message ID equality"
       (testing "true with no arguments"
         (is (jms/message-ids-equal?)))
